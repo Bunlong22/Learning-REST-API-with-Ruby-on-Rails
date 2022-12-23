87b279b4b7b173ae932600d05130b4f97a2d60e8
bunlong22 <suonbunlong93@gmail.com>
2022-12-23 16:25:18 +0700

    testing

---
+++ spec/factories/articles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    title { "Sample String"}
+    content { "Sample String"}
+    slug { "sample-string"}
+  end
+end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe "#validations" do  # group it
+  let (:article) { build(:article) }
+  it "test that factory is valid" do
+    # expect(1).to be_odd #expect 1 = odd, T, but if expect 2 = odd => failed
+     #can remove Factorybot cuz of config.include FactoryBot::Syntax::Method
+    expect(article).to be_valid #mean that if article == true
+  end
+  it 'has an invalid title' do
+    article.title= ''
+    expect(article).not_to be_valid
+    expect(article.errors[:title]).to include("can not be blank")
+  end
+end
+end 
+  
+# tesing file
